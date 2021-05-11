59eb99ec33b34261c1f9176ddbb6be0b973aeec4
ceddah <cedanik1@gmail.com>
2021-05-11 20:19:59 +0200

    Active Player's name is now Updated

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//Add option to change Names for P1 and P2 but let P1 and P2 be default if they are not changed.
-//Link first and section page with click event
+//Add return/choose mark again button in second screen that returns to first screen
 const gameSettings = (() => {
     const createPlayer = (name, mark, turn) => {
         return {name,
@@ -7,7 +6,10 @@ const gameSettings = (() => {
                 turn,
                 changeTurns() {
                     this.turn = !this.turn
-        }}
+                }, changeName(newName) {
+                        this.name = newName;
+                }
+        }
     };
     const Player1 = createPlayer('Player1', 'X', true);
     const Player2 = createPlayer('Player2', 'O', false);
@@ -32,14 +34,26 @@ const gameSettings = (() => {
         [0,4,8]
     ];
 
-    const gameStart = () => {
+    const Player1Input = document.querySelector('#player1Name');
+    const Player2Input = document.querySelector('#player2Name');
+
+    //Choose who starts the game first and Change default names
+    const startGame = () => {
         selections.forEach(button => {
-            if(button.classList.contains('selected')) {
+            if(button.classList.contains('selected') && Player1Input.value !== '' && Player2Input.value !== '') {
                 document.body.classList.add('active');
-            }
+                Player1.changeName(Player1Input.value);
+                Player2.changeName(Player2Input.value);
+                document.querySelector('.turn').innerHTML = `${Player1.name}'s turn:`
+                if(button.textContent === 'O') {
+                    Player1.changeTurns();
+                    Player2.changeTurns();
+                    document.querySelector('.turn').innerHTML = `${Player2.name}'s turn:`
+                }
+            } 
         })
     }
-
+    //Event Listener for Selecting your Mark
     const selections = document.querySelectorAll('.options button');
         selections.forEach(button => button.addEventListener('click', (e) => {
             selections.forEach(button => button.classList.remove('selected'));
@@ -47,7 +61,7 @@ const gameSettings = (() => {
         }))
 
     const startButton = document.querySelector('.game-start');
-    startButton.addEventListener('click', gameStart);
+    startButton.addEventListener('click', startGame);
 
     return {board, Player1, Player2, winningCombination, newBoard}
 })()
@@ -66,25 +80,24 @@ const gameController = (() => {
         markField(event)
     }));
 
+    //What happends when we click on the board
     const markField = (e) => {
         const index = e.target.dataset.id;
         turnsLeft -= 1;
         e.currentTarget.classList.add('occupied');
         
         if(gameSettings.Player1.turn === true && gameSettings.Player2.turn === false) {
-            // e.currentTarget.innerHTML = gameSettings.Player1.mark;
             e.currentTarget.innerHTML = `<span>${gameSettings.Player1.mark}</span>`;
             gameSettings.Player1.changeTurns()
             gameSettings.Player2.changeTurns();
             gameSettings.board[index] = gameSettings.Player1.mark;
-            turnElement.innerHTML = "Player 2's turn:"
+            turnElement.innerHTML = `${gameSettings.Player2.name}'s turn:`;
         } else {
-            // e.currentTarget.innerHTML = gameSettings.Player2.mark;
             e.currentTarget.innerHTML = `<span>${gameSettings.Player2.mark}</span>`;
             gameSettings.Player1.changeTurns()
             gameSettings.Player2.changeTurns();
             gameSettings.board[index] = gameSettings.Player2.mark;
-            turnElement.innerHTML = "Player 1's turn:"
+            turnElement.innerHTML = `${gameSettings.Player1.name}'s turn:`;
         }
         checkForWinner();
 
@@ -98,11 +111,11 @@ const gameController = (() => {
 
     const setFinalMessage = (winner) => {
         switch(winner) {
-            case 'Player1':
-                finalMessage = 'Player 1 has won!';
+            case `${gameSettings.Player1.name}`:
+                finalMessage = `${gameSettings.Player1.name} has won!`;
                 break;
-            case 'Player2':
-                finalMessage = 'Player 2 has won!';
+            case `${gameSettings.Player2.name}`:
+                finalMessage = `${gameSettings.Player2.name} has won!`;
                 break;
             default:
                 finalMessage = 'Game is Tied.'
@@ -111,10 +124,10 @@ const gameController = (() => {
     }
 
     const highLightCombo = (combination, fields, winner) => {
-        if(winner === 'Player1') {
-            combination.forEach(combo => fields[combo].classList.add('player1Won'));
-        } else if (winner === 'Player2'){
-            combination.forEach(combo => fields[combo].classList.add('player2Won'));
+        if(winner === 'X') {
+            combination.forEach(combo => fields[combo].classList.add('redSideWins'));
+        } else if (winner === 'O'){
+            combination.forEach(combo => fields[combo].classList.add('blueSideWins'));
         } else { 
             return 
         }
@@ -125,15 +138,15 @@ const gameController = (() => {
         gameSettings.winningCombination.forEach(combo => {
             if(gameSettings.board[combo[0]] === gameSettings.Player1.mark && gameSettings.board[combo[1]] === gameSettings.Player1.mark && gameSettings.board[combo[2]] === gameSettings.Player1.mark) {
                 winner = gameSettings.Player1;
-                setFinalMessage(winner.name);
+                setFinalMessage(gameSettings.Player1.name);
                 declareWinner();
-                highLightCombo(combo, fields, winner.name);
+                highLightCombo(combo, fields, winner.mark);
             } else if(gameSettings.board[combo[0]] === gameSettings.Player2.mark && gameSettings.board[combo[1]] === gameSettings.Player2.mark && gameSettings.board[combo[2]] === gameSettings.Player2.mark) {
                 winner = gameSettings.Player2;
-                setFinalMessage(winner.name);
+                setFinalMessage(gameSettings.Player2.name);
                 declareWinner();
-                highLightCombo(combo, fields, winner.name);
-            } else if(turnsLeft < 1){
+                highLightCombo(combo, fields, winner.mark);
+            } else if(turnsLeft < 1 && winner === undefined){
                 winner = 'Tied'
                 setFinalMessage(winner);
                 declareWinner();
@@ -152,21 +165,19 @@ const gameController = (() => {
         finalMessage = undefined;
         boardElement.classList.remove('gameIsOver');
         endGameEl.classList.add('hidden');
-        turnElement.innerHTML = "Player 1's turn:"
+        turnElement.innerHTML = `${gameSettings.Player1.turn === true ? gameSettings.Player1.name : gameSettings.Player2.name}'s turn:`;
         turnElement.classList.remove('hidden');
-        gameSettings.Player1.turn = true;
-        gameSettings.Player2.turn = false;
     }
 
-    const resetBtn = document.querySelector('.reset');
-    resetBtn.addEventListener('click', () => {
+    const playAgain = document.querySelector('.play-again-btn');
+    playAgain.addEventListener('click', () => {
         gameRestart();
         fields.forEach(field => {
             field.innerHTML = '';
             field.classList.remove('occupied');
             field.classList.add('anim');
-            field.classList.remove('player1Won');
-            field.classList.remove('player2Won');
+            field.classList.remove('redSideWins');
+            field.classList.remove('blueSideWins');
             turnsLeft = 9;
             setTimeout(() => {
                 field.classList.remove('anim');
